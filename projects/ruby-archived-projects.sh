393379da960304edd06a91665af2cf6b2a1eaeb7
Scott Bellware <sbellware@gmail.com>
2016-05-11 01:30:55 -0400

    File naming standards are applied

---
+++ projects/ruby-archived-projects.sh
@@ -0,0 +1,11 @@
+set -e
+
+ruby_archived_projects=(
+  "error-data"
+  "error-telemetry-component"
+  "eventide-rubygems-org-placeholder"
+  "metrics-stream-divergence"
+  "ops-genie-client"
+  "raygun-client"
+  "stream-monitor"
+)
