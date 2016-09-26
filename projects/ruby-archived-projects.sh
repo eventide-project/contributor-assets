080e2c07790d580871eeb4d82ef32f3c29d3d8c1
Scott Bellware <sbellware@gmail.com>
2016-09-26 10:28:10 -0500

    The event-source-postgres and event-source projects are includes in the list of Ruby public gem projects The event-stream-postgres project is archived

--- projects/ruby-archived-projects.sh
+++ projects/ruby-archived-projects.sh
@@ -3,6 +3,7 @@ set -e
 ruby_archived_projects=(
   "error-data"
   "error-telemetry-component"
+  "event-stream-postgres"
   "eventide-rubygems-org-placeholder"
   "metrics-stream-divergence"
   "ops-genie-client"
