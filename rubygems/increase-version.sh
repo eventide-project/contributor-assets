031216ff88f16054ff3fd197e47ea9803d3f4101
Scott Bellware <sbellware@gmail.com>
2017-05-31 11:59:35 -0500

    Script dir is referenced as an absolute path

--- rubygems/increase-version.sh
+++ rubygems/increase-version.sh
@@ -6,26 +6,46 @@ set -e
 
 exec 1>&2
 
-echo "This line will appear in $LOG_FILE, not 'on screen'"
-
 if [ -z ${PROJECTS_HOME+x} ]; then
-  echo "PROJECTS_HOME is not set" >&2
+  echo "PROJECTS_HOME is not set"
   exit
 fi
 
 if [ -z ${LEVEL+x} ]; then
-  echo "LEVEL is not set" >&2
+  echo "LEVEL is not set"
   exit
 fi
 
 source ./projects/ruby-public-gem-projects.sh
 source ./utilities/run-cmd.sh
 
+script_dir=$(pwd)
+
 function increase-version {
   gemspec=$1
   level=$2
 
-  echo "LEVEL=$level GEMSPEC=$gemspec ruby increase_version.rb" >&2
+  increase_version_ruby_script="$script_dir/rubygems/increase_version.rb"
+
+  versions=($(LEVEL=$level GEMSPEC=$gemspec ruby $increase_version_ruby_script))
+
+  current_version=${versions[0]}
+  next_version=${versions[1]}
+
+  commit-changes $current_version $next_version
+}
+
+function commit-changes {
+  current_version=$1
+  next_version=$2
+
+  commit_message="Package version is increased from $current_version to $next_version"
+
+  add_cmd="git add ."
+  run-cmd "$add_cmd"
+
+  commit_cmd="git commit -m '$commit_message'"
+  run-cmd "$commit_cmd"
 }
 
 echo
