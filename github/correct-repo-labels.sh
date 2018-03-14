de47648f30d9b681a84aeee5f77bd723c307013f
Scott Bellware <sbellware@gmail.com>
2018-03-14 15:54:46 -0500

    Help wanted label is removed

--- github/correct-repo-labels.sh
+++ github/correct-repo-labels.sh
@@ -8,9 +8,11 @@ function correct-repo-labels {
   echo "$repo"
   echo "- - -"
 
-  echo "Changing entry level label"
-  data='{"name":"entry level"}'
-  curl -s -u $github_token:x-oauth-basic -H "Accept: application/vnd.github.symmetra-preview+json" --include --request PATCH --data "$data" "https://api.github.com/repos/$org_name/$repo/labels/good%20first%20issue" > /dev/null
+  # data='{"name":"entry level"}'
+  # curl -s -u $github_token:x-oauth-basic -H "Accept: application/vnd.github.symmetra-preview+json" --include --request PATCH --data "$data" "https://api.github.com/repos/$org_name/$repo/labels/good%20first%20issue" > /dev/null
+
+  echo "Removing help wanted label"
+  curl -s -u $github_token:x-oauth-basic -H "Accept: application/vnd.github.v3+json" --request DELETE "https://api.github.com/repos/$org_name/$repo/labels/help%20wanted" > /dev/null
 
   echo
 }
