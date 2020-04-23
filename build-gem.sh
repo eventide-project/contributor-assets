6cf484ec817eb309e26b99a32f12bce86003250c
Nathan Ladd <nathanladd@gmail.com>
2020-04-23 10:53:45 -0500

    Build gem script uses remove-symlinks.sh

--- build-gem.sh
+++ build-gem.sh
@@ -4,36 +4,19 @@ set -eu
 
 function buildGem() {
   local gemspec=$1
+  local symlinks
 
   echo
   echo "Building gem: $gemspec"
   echo "= = ="
   echo
 
-  removeSymlinks $LIBRARIES_HOME
-
   gem build $gemspec
 
   echo
 }
 
-function removeSymlinks() {
-  local dir=$1
-  local entry;
-
-  for entry in $dir/*; do
-    if [ ! -L $entry ]; then
-      continue
-    fi
-
-    if [ -d $entry ]; then
-      removeSymlinks $entry
-    fi
-
-    echo "- removing symlink: $entry"
-    rm -f $entry
-  done
-}
+eval "$(dirname $BASH_SOURCE)/symlinks/remove-symlinks.sh"
 
 for gem in *.gemspec; do
   buildGem $gem
